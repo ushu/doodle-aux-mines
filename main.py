7a6f24754640f5d8e3fa06ea91aa5c15bf17aa12
Aurélien Noce <aurelien@noce.fr>
2025-02-24 15:47:36 +0100

    ♻️ Modèle de code vu en cours
    Ce patch met en place un moteur de template et une gestion des fichiers
    statiques identiques à l'exemple vu au premier cours.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Static files are handled by a specific sub-router monted on /static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# We instanciate a template engine for jinja2
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def home():
-    return HTMLResponse(f"""
-    <html>
-        <body>
-            <h1>Hello from doodle-aux-mines!</h1>
-        </body>
-    </html>
-    """)
+def home(request: Request):
+    return templates.TemplateResponse(
+        name="index.html", 
+       request=request
+    )
